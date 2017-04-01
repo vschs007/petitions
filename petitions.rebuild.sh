cc6d57bc804854fe5e09de03fad13b3b24f8b8dc
Vivek Kumar Singh <vschs007@gmail.com>
2017-04-01 14:55:07 +0530

    updated
    space efficient

--- petitions.rebuild.sh
+++ petitions.rebuild.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo "Rebuilding..."
+echo "Rebuilding.."
 rm -rf modules/contrib themes/zen themes/fortyfour libraries/
 drush -y make --no-core --contrib-destination=. drupal-org.make
